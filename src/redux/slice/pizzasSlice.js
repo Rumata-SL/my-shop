f07505552b91952dcdf0e928feb58df96c9780c7
Rumata-SL <kirr7619@gmail.com>
2022-07-21 23:47:05 +0500

    added redux and redux-toolkit

---
+++ src/redux/slice/pizzasSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    value: 0,
+}
+
+export const pizzasSlice = createSlice({
+    name: 'pizzas',
+    initialState,
+    reducers: {
+        increment: (state) => {
+
+            state.value += 1
+        },
+        decrement: (state) => {
+            state.value -= 1
+        },
+        incrementByAmount: (state, action) => {
+            state.value += action.payload
+        },
+    },
+})
+
+
+export const { increment, decrement, incrementByAmount } = pizzasSlice.actions
+
+export default pizzasSlice.reducer
