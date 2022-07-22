90f9a536bbdea90ae6a1ac55e93d6d37f0401111
Rumata-SL <kirr7619@gmail.com>
2022-07-22 14:41:56 +0500

    added Lodash, added useCallback, useRef

--- src/component/search/Serch.jsx
+++ src/component/search/Serch.jsx
@@ -1,22 +1,44 @@
-import React, {useContext} from 'react';
+import React, {useCallback, useContext, useRef, useState} from 'react';
 import style from "./Serch.module.scss"
 import iconSearch from "./iconSearch.svg"
 import iconClear from "./iconClear.svg"
 import {SearchContext} from "../../App";
+import debounce from "lodash.debounce"
 
 export const Search = () => {
-    const {searchValue, setSearchValue} = useContext(SearchContext)
+    const [value, setValue] = useState("")
+    const {setSearchValue} = useContext(SearchContext)
+    const inputRef = useRef()
+
+    const onClickClear = () => {
+        setSearchValue("")
+        setValue("")
+        inputRef.current.focus()
+    }
+
+    const updateSearchValue = useCallback(
+        debounce((str) => {
+            setSearchValue(str)
+        }, 1000), []
+    )
+    const onChangeInput = (event) => {
+        setValue(event.target.value)
+        updateSearchValue(event.target.value)
+    }
 
     return (
         <div className={style.root}>
             <img className={style.icon} src={iconSearch} alt="iconSearch"/>
 
-            <input value={searchValue}
-                   onChange={(e) => setSearchValue(e.target.value)}
+            <input value={value}
+                   ref={inputRef}
+                   onChange={onChangeInput}
                    className={style.input} type="text"
                    placeholder="Поиск пиццы..."/>
 
-            {searchValue && <img src={iconClear} alt="iconClear" onClick={() => setSearchValue('')} className={style.clearIcon}/>}
+            {value &&
+                <img src={iconClear} alt="iconClear" onClick={onClickClear}
+                     className={style.clearIcon}/>}
         </div>
     );
 };
--- src/redux/slice/filterSlice.js
+++ src/redux/slice/filterSlice.js
@@ -2,6 +2,7 @@ import {createSlice} from '@reduxjs/toolkit'
 
 const initialState = {
     categoryId: 0,
+    currentPage: 1,
     sort: {
         name: "популярности",
         sortProperty: "rating"
@@ -13,16 +14,19 @@ export const filterSlice = createSlice({
     initialState,
     reducers: {
         setCategoryId: (state, action) => {
-                state.categoryId = action.payload
+            state.categoryId = action.payload
         },
-        setSortType:(state,action)=>{
+        setSortType: (state, action) => {
             state.sort = action.payload
+        },
+        setCurrentPage: (state, action) => {
+            state.currentPage = action.payload
         }
 
     },
 })
 
 // Action creators are generated for each case reducer function
-export const {setCategoryId, setSortType} = filterSlice.actions
+export const {setCategoryId, setSortType, setCurrentPage} = filterSlice.actions
 
 // export default filterSlice.reducer
