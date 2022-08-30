ac1d461ad8a89714bf9372bc07ece6f5f8532ba6
Rumata-SL <kirr7619@gmail.com>
2022-08-30 17:16:43 +0500

    deleted context

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,12 +3,12 @@ import con from "../scss/app.scss"
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {CartItem} from "../component/cartItem";
-import {clearItems} from "../redux/slice/cartSlice";
+import {clearItems, selectCart} from "../redux/slice/cartSlice";
 import {CartEmpty} from "../component/cartEmpty";
 
 export const Cart = () => {
     const dispatch = useDispatch()
-    const {totalPrice, items} = useSelector(state => state.cart)
+    const {totalPrice, items} = useSelector(selectCart)
     const totalCount = items.reduce((acc, item)=>{
         return acc+item.count
     }, 0)
